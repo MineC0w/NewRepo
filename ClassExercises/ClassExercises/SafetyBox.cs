6b8fd2d85bd62e6ab138ac176721c865d5346414
MineC0w <lilcowgames@gmail.com>
2015-11-17 19:24:09 +0200

    Added questions

---
+++ ClassExercises/ClassExercises/SafetyBox.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    public class SafetyBox
+    {
+        private SafetyBoxStatus _status;
+        private double _money;
+
+        public SafetyBox()
+        {
+            _status = SafetyBoxStatus.Closed;
+            _money = 0;
+        }
+
+        public SafetyBoxStatus Status()
+        {
+            return _status;
+        }
+
+        public void Close()
+        {
+            _status = SafetyBoxStatus.Closed;
+        }
+
+        public void Open()
+        {
+            _status = SafetyBoxStatus.Open;
+        }
+
+        private void changeMoney(double money)
+        {
+            _money += money;
+        }
+
+        public void Deposit(double money)
+        {
+            if(_status == SafetyBoxStatus.Open)
+            changeMoney(money);
+        }
+        public double Withdraw(double money)
+        {
+            if(_status == SafetyBoxStatus.Open)
+            {
+                changeMoney(-money);
+                return money;
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            if(_status == SafetyBoxStatus.Open)
+                return string.Format("An open safe that contains {0} money.",_money);
+            return "The safe appears to be closed.";
+        }
+
+    }
+
+    public enum SafetyBoxStatus { Open, Closed}
+}
---
+++ ClassExercises/ClassExercises/Taxi.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    public class Taxi
+    {
+        private int _taxiId;
+        private string _driverName;
+        private int _numPass;
+        private int _currentPass;
+        private bool _isAvailable;
+
+        public Taxi(string driverName, int taxiId)
+        {
+            _taxiId = taxiId;
+            _driverName = driverName;
+            _numPass = 4;
+            _currentPass = 0;
+            _isAvailable = true;
+        }
+
+        public int getId()
+        {
+            return _taxiId;
+        }
+
+        public string getName()
+        {
+            return _driverName;
+        }
+
+        public int getNumPass()
+        {
+            return _numPass;
+        }
+
+        public bool isAvailable()
+        {
+            return _isAvailable;
+        }
+
+        public void setCurrentPass(int p)
+        {
+            _currentPass = p;
+        }
+
+        public void taxiBusy()
+        {
+            _currentPass = _numPass;
+            _isAvailable = false;
+        }
+
+        public int numberOfTaxi(int pass)
+        {
+            return pass / _numPass;
+        }
+
+        public int addPassenger(int pass)
+        {
+            return 4 - (pass - numberOfTaxi(pass) * _numPass);
+        }
+    }
+}
